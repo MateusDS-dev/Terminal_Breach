fbd0d2349ae19c51270b62953200757032bf4d6a
Luisvilas <luisvboas@outlook.com>
2026-05-05 15:58:49 -0300

    Update leaderboard.h

--- leaderboard.h
+++ leaderboard.h
@@ -5,7 +5,7 @@
 /*  Funções do placar de líderes                                        */
 /* ------------------------------------------------------------------ */
 
-/* Lê todas as sessões e exibe o TOP 5 em tabela ASCII. */
+/* Lê todas as sessões e exibe o TOP 10 em tabela ASCII. */
 void leaderboard_exibir(void);
 
 #endif /* LEADERBOARD_H */
